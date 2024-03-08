d9d8182c83a4b1d362d3106fda33f22c168aba83
Tilahun Demissie <til.assf98@gmail.com>
2024-03-08 06:24:49 -0600

    initial commit

---
+++ add translation order script.sql
@@ -0,0 +1,4 @@
+INSERT INTO `wmktranslation`.`translationorder`
+(`TranslationOrderId`,`OrderDate`,`TranslationType`,`Price`,`LanguageId`,`CustomerId`,`TranslatorId`)
+VALUES
+('371b93f2-f8c5-4a32-894a-fc672741aa5b','2024-01-09','construction bill of material', 120, '84fd81f8-d58a-4bdc-9f36-dc6cd5641906', '84fd81f8-d58a-4bdc-8f36-dc6cd5647906','24fd81f8-d58a-4bcc-9f35-dc6cd5641906')
